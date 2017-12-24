bd95b654ef6715a022189899294c15b5a33bf23e
Artem <sam_glad@mail.ru>
2017-12-24 17:01:52 +0300

    Realized steps 1-3 from "Task №2"

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,18 +1,20 @@
 package ru.stqa.pft.sandbox;
 
-public class Point {
-  public double x;
-  public double y;
-  public Point (double x, double y){
+public class Point {  // Создаём класс Point (Точка с параметрами x и y)
+  double x;  //Объявляем переменную для координаты x
+  double y; //Объявляем переменную для координаты y
+  public Point (double x, double y){ // Создаём конструктор для класса Point для обработки конкретных точек
+    this.x=x;
+    this.y=y;
         }
 
-  public static void main(String[] args) {
-    Point pu1 = new Point(3,3);
-    Point pu2 = new Point(2,2);
-    System.out.println("Расстояние между точками p1 и p2 = " + distance(pu1, pu2));
+  public static void main(String[] args) { //Создаём функцию для вывода результа вычисления на экран
+    Point p1 = new Point(3,3); //Задаём координаты первой точки
+    Point p2 = new Point(2,2); //Задаём координаты второй точки
+    System.out.println("Расстояние между точками p1 и p2 = " + distance(p1, p2)); //Выводим результат на экран
   }
 
-  public static double distance(Point p1, Point p2){
+  public static double distance(Point p1, Point p2){ //Создаём функцию вычисления расстояния между двумя точками
     double dx = p1.x - p2.x;
     double dy = p1.y - p2.y;
     return Math.sqrt(dx*dx + dy*dy);
