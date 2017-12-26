a09a789152b90e2c05d7ee92bec258302a359663
Artem <sam_glad@mail.ru>
2017-12-26 20:14:44 +0300

    Implemented stepps 1, 2, 3
    Реализованы шаги 1, 2, 3

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,15 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    Point p1 = new Point(7, 2); //Задаём координаты первой точки
+    Point p2 = new Point(4, 1); //Задаём координаты второй точки
+    System.out.println("Расстояние между точками p1 и p2 = " + distance(p1, p2)); //Выводим результат на экран
   }
 
+  public static double distance(Point p1, Point p2) { //Создаём функцию вычисления расстояния между двумя точками
+    double dx = p1.x - p2.x;
+    double dy = p1.y - p2.y;
+    return Math.sqrt(dx * dx + dy * dy);
+  }
 }
+
