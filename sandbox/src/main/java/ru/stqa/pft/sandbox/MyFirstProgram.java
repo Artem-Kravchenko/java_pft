4b47a17b760f7ef64859c86473aebadec8de137f
Artem <sam_glad@mail.ru>
2017-12-27 20:18:44 +0300

    Implemented step 4 from task №2
    Successfully implemented step 4 from task №2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,15 +3,10 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    Point p1 = new Point(7, 2); //Задаём координаты первой точки
-    Point p2 = new Point(4, 1); //Задаём координаты второй точки
-    System.out.println("Расстояние между точками p1 и p2 = " + distance(p1, p2)); //Выводим результат на экран
-  }
-
-  public static double distance(Point p1, Point p2) { //Создаём функцию вычисления расстояния между двумя точками
-    double dx = p1.x - p2.x;
-    double dy = p1.y - p2.y;
-    return Math.sqrt(dx * dx + dy * dy);
+    Point p1 = new Point(7, 2); //Создаём первый объект типа точка
+    Point p2 = new Point(4, 5); //Создаём второй объект типа точка
+    System.out.println("Расстояние между точками p1 и p2 = " + p1.distance(p2)); /* Используем метод distance из класса Point для
+                                                                                 расчёта расстояние между точками и выводим результат
+                                                                                 на экран */
   }
 }
-
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -3,8 +3,14 @@ package ru.stqa.pft.sandbox;
 public class Point { // Создаём класс Point для представления точек на двумерной плоскости
   public double x;
   public double y;
-  public Point(double x, double y) { // Создаём конструктор для передачи конкретных данных в функцию расчёта
+  public Point(double x, double y) { // Создаём конструктор для передачи данных первой точки в функцию расчёта
     this.x = x;
     this.y = y;
+    }
+
+  public double distance(Point p2) { //Создаём метод, в котором вычисляются расстояние между двумя точками (От текущей до переданной)
+    double dx = this.x - p2.x;
+    double dy = this.y - p2.y;
+    return Math.sqrt(dx * dx + dy * dy);
   }
 }
