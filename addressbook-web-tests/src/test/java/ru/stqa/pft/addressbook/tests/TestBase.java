dde796a4dbf3e05a4745cb64b830b01336f78577
Artem <sam_glad@mail.ru>
2018-01-05 21:16:25 +0300

    Implemented steps 1-3 for task 5 (Groups only). Partially implemented step 1 for contacts

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,27 +1,33 @@
-package ru.stqa.pft.addressbook;
+package ru.stqa.pft.addressbook.tests;
 
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+import ru.stqa.pft.addressbook.model.ContactData;
 
-public class ContactCreationTests {
+public class TestBase {
+
+  protected final ApplicationManager app = new ApplicationManager();
   FirefoxDriver wd;
+
+  public static boolean isAlertPresent(FirefoxDriver wd) {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
 
   @BeforeMethod
   public void setUp() throws Exception {
-    wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/");
-    login("admin", "secret");
+    app.init();
   }
 
-  private void login(String username, String password) {
+  protected void login(String username, String password) {
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys(username);
@@ -31,23 +37,15 @@ public class ContactCreationTests {
     wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
   }
 
-  @Test
-  public void testContactCreation() {
-    gotoContactPage();
-    fillContactForm(new ContactData("John", "Smith", "New York City", "89001234567", "email@email.com"));
-    submitContactCreation();
-    returnToHomePage();
-  }
-
-  private void returnToHomePage() {
+  protected void returnToHomePage() {
     wd.findElement(By.linkText("home")).click();
   }
 
-  private void submitContactCreation() {
+  protected void submitContactCreation() {
     wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
   }
 
-  private void fillContactForm(ContactData contactData) {
+  protected void fillContactForm(ContactData contactData) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
     wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
@@ -65,21 +63,13 @@ public class ContactCreationTests {
     wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
   }
 
-  private void gotoContactPage() {
+  protected void gotoContactPage() {
     wd.findElement(By.linkText("add new")).click();
   }
 
   @AfterMethod
-  public void tearDown() {
-    wd.quit();
+    public void tearDown() {
+    app.stop();
   }
 
-  public static boolean isAlertPresent(FirefoxDriver wd) {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
 }
