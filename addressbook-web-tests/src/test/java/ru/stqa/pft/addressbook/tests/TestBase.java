ab25ee9e155fca543f6fd49062ec34829961f620
Artem <sam_glad@mail.ru>
2018-01-27 17:27:04 +0300

    Lection 5.1

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,20 +1,21 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX); //Делаем переменную app глобальной
+  // т.е. доступной всем классам
 
-  @BeforeMethod
+  @BeforeSuite // Запускаем браузер один раз перед всеми нужными тестами
   public void setUp() throws Exception {
     app.init();
   }
 
-  @AfterMethod
+  @AfterSuite // Останаливаем браузер один раз после всех нужных тестов
     public void tearDown() {
     app.stop();
   }
