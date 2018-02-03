25b36bb00c1f196a44a0b4177549223c26eb4b40
Artem <sam_glad@mail.ru>
2018-02-03 16:57:31 +0300

    Lection 6.5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -15,11 +16,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
   @DataProvider //Провайдер тестовыъ данных
-  public Iterator<Object[]> validGroups() { //Список массивов объектов
+  public Iterator<Object[]> validGroups() throws IOException { //Список массивов объектов
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[] {new GroupData().withName("test 1").withHeader("header 1").withFooter("footer 1")}); //Элемент массива
-    list.add(new Object[] {new GroupData().withName("test 2").withHeader("header 2").withFooter("footer 2")}); //Элемент массива
-    list.add(new Object[] {new GroupData().withName("test 3").withHeader("header 3").withFooter("footer 3")}); //Элемент массива
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv"))); //Чтение данных для теста из файла
+    String line = reader.readLine(); // Чтение строки из файла
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])}); //В список добавляем массив объектов типа GroupData
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
