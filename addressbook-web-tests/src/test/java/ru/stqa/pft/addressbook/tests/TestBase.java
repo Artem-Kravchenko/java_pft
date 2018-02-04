319c856dc9a721342bb1ba59b45e8f09fe42521e
Artem <sam_glad@mail.ru>
2018-02-04 16:16:53 +0300

    Lection 6.11 (Logging current events)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,14 +1,23 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
 
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
+
   protected static final ApplicationManager app
-          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME)); //Делаем переменную app глобальной
+          = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX)); //Делаем переменную app глобальной
   // т.е. доступной всем классам
 
   @BeforeSuite // Запускаем браузер один раз перед всеми нужными тестами
@@ -16,9 +25,19 @@ public class TestBase {
     app.init();
   }
 
-  @AfterSuite // Останаливаем браузер один раз после всех нужных тестов
+  @AfterSuite(alwaysRun = true) // Останаливаем браузер один раз после всех нужных тестов
     public void tearDown() {
     app.stop();
+  }
+
+  @BeforeMethod
+  public void logTestStart(Method m, Object[] p) {
+    logger.info("Start test " + m.getName() + "with parameters" + Arrays.asList(p));
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method m) {
+    logger.info("Stop test " + m.getName());
   }
 
 }
