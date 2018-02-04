6ec79f87490b5e0a69955a0810d2aecff51d84e7
Artem <sam_glad@mail.ru>
2018-02-04 15:06:51 +0300

    Lection 6.9 (For contacts and groups)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,18 +1,13 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
-import java.util.*;
-
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.*;
-import static org.testng.Assert.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupModificationTests extends TestBase {
 
@@ -33,7 +28,8 @@ public class GroupModificationTests extends TestBase {
     app.group().modify(group); //Выбор и модификация конкретной группы по id
     assertThat(app.group().count(), equalTo(before.size()));
     Groups after = app.group().all();
-    assertThat(after, equalTo(before.withOut(group).withAdded(group)));
+    assertThat(after, equalTo(before.withOut(modifiedGroup).withAdded(group)));
+
    }
 
 }
