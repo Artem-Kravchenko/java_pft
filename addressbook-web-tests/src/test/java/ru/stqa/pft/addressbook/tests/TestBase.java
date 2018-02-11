628565fcc291d45a0583d946dc63a34333ada35c
Artem <sam_glad@mail.ru>
2018-02-11 13:55:03 +0300

    Lection 7.5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,16 +1,25 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestBase {
 
@@ -40,4 +49,13 @@ public class TestBase {
     logger.info("Stop test " + m.getName());
   }
 
+  public void verifyGroupListInUI() { //Функция для сравнения списков из БД и UI
+    if (Boolean.getBoolean("verifyUI")) { //Системное свойство, которое определяет, включена эта функция или нет
+      Groups dbGroups = app.db().groups(); //Извлечение списка групп из БД
+      Groups uiGroups = app.group().all(); //Извлечение списка групп из UI
+      assertThat(uiGroups, equalTo(dbGroups.stream()
+              .map((g) -> new GroupData().withId(g.getId()).withName(g.getName()))
+              .collect(Collectors.toSet()))); //Сравнение двух объектов, у которых есть только id и имя
+    }
+  }
 }
