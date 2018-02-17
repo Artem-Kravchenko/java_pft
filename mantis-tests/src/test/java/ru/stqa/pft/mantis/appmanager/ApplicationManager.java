398885b7e50198143ddb94e8d3f169a9e6901c15
Artem <sam_glad@mail.ru>
2018-02-17 14:51:40 +0300

    Lection 8.4

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import ru.stqa.pft.mantis.tests.RegistrationTests;
 
 import java.io.File;
 import java.io.FileReader;
@@ -16,9 +17,10 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationManager {
 
   private final Properties properties;
-  WebDriver wd; //Указываем тип интерфейса
+  private WebDriver wd; //Указываем тип интерфейса
 
   private String browser; //Поле (переменная) browser
+  private RegistrationHelper registrationHelper;
 
   public ApplicationManager(String browser) { //Конструктор класса ApplicationManager с параметром browser
     this.browser = browser;                   //(Через этот параметр передаётся конкретный драйвер)
@@ -29,29 +31,42 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-     if (browser.equals(BrowserType.FIREFOX)) { //Выбор браузера со сравнением через метод equals
-      wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-    } else if (browser.equals(BrowserType.CHROME)) {
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.IE)) {
-      wd = new InternetExplorerDriver();
-    }
-
-    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseUrl"));
-     }
-
-
-  public void stop() {
-    wd.quit();
   }
 
-  public HttpSession newSession(){
+
+  public void stop() { //Ленивая остановка драйвера
+    if (wd != null) {
+      wd.quit();
+    }
+  }
+
+  public HttpSession newSession() {
     return new HttpSession(this);
   }
 
   public String getProperty(String key) {
     return properties.getProperty(key);
+  }
+
+  public RegistrationHelper registration() { //Ленивая инициализация объекта класса RegistrationHelper
+    if (registrationHelper == null) {
+      registrationHelper = new RegistrationHelper(this);
+    }
+    return registrationHelper;
+  }
+
+  public WebDriver getDriver() { //Ленивая инициализация веб-драйвера (Чтобы драйвер инициализировался тогда, когда он нам нужен)
+    if (wd == null) {
+      if (browser.equals(BrowserType.FIREFOX)) { //Выбор браузера со сравнением через метод equals
+        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+      } else if (browser.equals(BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.IE)) {
+        wd = new InternetExplorerDriver();
+      }
+      wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+      wd.get(properties.getProperty("web.baseUrl"));
+    }
+    return wd;
   }
 }
